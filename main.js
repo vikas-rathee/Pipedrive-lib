d2286c187bf4052ff6fce4e18e87754bde8c34dd
Vikas Rathee <vikasrathee10@gmail.com>
2019-09-22 09:35:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ var bodyParser = require("body-parser");
 var apiToken = "d8e1d4de076120a74f145416bd11d9f28f13459f";
 var app = express();
 
-// require("./addSVStopField");
-
 app.use(bodyParser.json())
    .use(bodyParser.urlencoded({ extended: true}))
    .use("/api", routes)
