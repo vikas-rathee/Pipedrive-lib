b3148ad0add4142c9b9b9447fa764374b6e6b580
Vikas Rathee <vikasrathee10@gmail.com>
2019-09-22 10:14:21 +0530

    Update notesController.js

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -2,10 +2,10 @@ var Pipedrive = require("pipedrive");
 var pd = new Pipedrive.Client("d8e1d4de076120a74f145416bd11d9f28f13459f", {strictMode : true});
 
 module.exports.addNotes = function(req, res){
-  pd.Notes.add(req.body, function(err, note){
+  pd.Notes.add(req.body, function(err, notes){
     if(err)
       throw(err);
-    res.json(note);
+    res.json(notes);
   })
 };
 
